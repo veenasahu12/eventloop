d3825068904eb4614952de0695043aca179a2428
veenasahu12 <veenasahusbp@gmail.com>
2022-03-06 12:13:38 +0530

    eventloop

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/books",function(req,res){
+    console.log("hello");
+
+    res.send({
+        BOOK1: "War and Peace",
+        BOOK2: "Tom Jones",
+        BOOK3: "Nightmare Abbey",
+        BOOK4: "Sybil",
+    });
+});
+
+app.listen(5000,() => {
+      console.log("5000 responded successfully");
+});
