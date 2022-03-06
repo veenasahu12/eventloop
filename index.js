ce7a694bb9e15b92746ece949d80b3ffb34cd98f
veenasahu12 <veenasahusbp@gmail.com>
2022-03-06 13:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const app = express();
 app.get("/books",function(req,res){
     console.log("hello");
 
-    res.send({
-        BOOK1: "War and Peace",
-        BOOK2: "Tom Jones",
-        BOOK3: "Nightmare Abbey",
-        BOOK4: "Sybil",
-    });
+    res.send([
+        {
+            BOOK1: "War and Peace"
+        },
+        {
+           BOOK2: "Tom Jones"
+        },
+        {
+            BOOK3: "Nightmare Abbey"
+        },
+        {
+            BOOK4: "Sybil"
+        },
+    ]);
 });
 
 app.listen(5000,() => {
